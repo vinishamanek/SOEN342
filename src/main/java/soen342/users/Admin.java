9fd6d0c5ee34d6725c094a96b8981a0342629ed1
mahutt <tommy.mahut@gmail.com>
2024-11-14 09:03:56 -0500

    Remove singleton pattern from admin class

--- src/main/java/soen342/users/Admin.java
+++ src/main/java/soen342/users/Admin.java
@@ -1,37 +1,18 @@
 package soen342.users;
-// admin implemented as singleton class for now because only 1 admin allowed
 
-import soen342.location.Space;
 import jakarta.persistence.*;
 import soen342.location.Organization;
-import soen342.reservation.Lesson;
-import soen342.reservation.TimeSlot;
 
 @Entity
 @Table(name = "admins")
 public class Admin extends User {
 
-    private static Admin instance = null;
-
     protected Admin() {
         super();
     }
 
-    private Admin(String email, String password, Organization organization) {
+    public Admin(String email, String password, Organization organization) {
         super(email, password, organization);
-    }
-
-    public static Admin getInstance(String email, String password, Organization organization) {
-        if (instance == null) {
-            instance = new Admin(email, password, organization);
-        }
-        return instance;
-    }
-
-    public void deleteAccount(User user) {
-        // admin can delete account of instructor or client
-        // method will be defined later on
-        // System.out.println("Account deleted for: " + user.getEmail());
     }
 
 }
