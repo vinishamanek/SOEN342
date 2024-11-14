3b817cef03be16c8fa7caae94d685da03b4f46d8
Vinisha Manek <Vinisha@exoshop.com>
2024-11-14 13:29:31 -0500

    refine city catalog

--- src/main/java/soen342/catalogs/CityCatalog.java
+++ src/main/java/soen342/catalogs/CityCatalog.java
@@ -1,14 +1,21 @@
 package soen342.catalogs;
 
 import java.util.List;
-
 import soen342.location.City;
 
 public class CityCatalog {
+    private static CityCatalog instance;
     private List<City> cities;
 
-    public CityCatalog(List<City> cities) {
+    private CityCatalog(List<City> cities) {
         this.cities = cities;
+    }
+
+    public static CityCatalog getInstance(List<City> cities) {
+        if (instance == null) {
+            instance = new CityCatalog(cities);
+        }
+        return instance;
     }
 
     public void setCities(List<City> cities) {
