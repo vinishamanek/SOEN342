79457ee1afd091d7f4c404db42f91746db96a892
mahutt <tommy.mahut@gmail.com>
2024-11-14 10:17:36 -0500

    Create user catalog

---
+++ src/main/java/soen342/catalogs/UserCatalog.java
@@ -0,0 +1,14 @@
+package soen342.catalogs;
+
+import soen342.users.User;
+
+import java.util.List;
+
+public class UserCatalog {
+
+    public List<User> users;
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
--- src/main/java/soen342/database/UserMapper.java
+++ src/main/java/soen342/database/UserMapper.java
@@ -4,9 +4,13 @@ import jakarta.persistence.NoResultException;
 import jakarta.persistence.TypedQuery;
 import java.util.List;
 
+import soen342.catalogs.UserCatalog;
 import soen342.users.User;
 
 public class UserMapper extends AbstractMapper<User> {
+
+    private UserCatalog userCatalog;
+
     public User findByEmail(String email) {
         try {
             TypedQuery<User> query = entityManager.createQuery(
@@ -19,6 +23,11 @@ public class UserMapper extends AbstractMapper<User> {
     }
 
     public User findByCredentials(String email, String password) {
+        // @todo popupate userCatalog using findALl() method
+        // @then search through the catalog to find the user with the email (implement
+        // findByEmail in catalog? or not even bother with that?)
+        // @then check if the password matches
+        // @note this is awfully inefficient
         try {
             TypedQuery<User> query = entityManager.createQuery(
                     "SELECT u FROM User u WHERE u.email = :email AND u.password = :password", User.class);
