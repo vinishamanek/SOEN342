8810eeaa1ec2618c5d3dab5a156c874dbb166088
Vinisha Manek <Vinisha@exoshop.com>
2024-11-14 13:27:54 -0500

    refined province catalog

--- src/main/java/soen342/catalogs/ProvinceCatalog.java
+++ src/main/java/soen342/catalogs/ProvinceCatalog.java
@@ -1,14 +1,21 @@
 package soen342.catalogs;
 
 import java.util.List;
-
 import soen342.location.Province;
 
 public class ProvinceCatalog {
     private List<Province> provinces;
+    private static ProvinceCatalog instance;
 
-    public ProvinceCatalog(List<Province> provinces) {
+    private ProvinceCatalog(List<Province> provinces) {
         this.provinces = provinces;
+    }
+
+    public static ProvinceCatalog getInstance(List<Province> provinces) {
+        if (instance == null) {
+            instance = new ProvinceCatalog(provinces);
+        }
+        return instance;
     }
 
     public void setProvinces(List<Province> provinces) {
