40e0c84dce88b62b893f45e13972205b45c9b621
mahutt <tommy.mahut@gmail.com>
2024-11-15 16:22:56 -0500

    Remove auto-populate

--- src/main/java/soen342/Driver.java
+++ src/main/java/soen342/Driver.java
@@ -5,8 +5,6 @@ import soen342.database.JPAUtil;
 
 public class Driver {
     public static void main(String[] args) {
-        Populate.main(null);
-
         JPAUtil.init();
         Console console = new Console(null);
         console.run();
